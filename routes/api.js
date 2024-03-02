5246cd2d6fce5b696f1afb78b0cf8a8234567b3d
imtiaz-cnits <ia64744@gmail.com>
2024-03-02 20:39:21 +0600

    added some apis

--- routes/api.js
+++ routes/api.js
@@ -1,14 +1,18 @@
 import express from "express";
-import * as todoController from "../app/controllers/todoController.js";
+import * as todoController from "../app/controllers/brandController.js";
 const router = express.Router();
-// import * as todoController from '../app/controllers/brandController.js';
-// import * as todoController from '../app/controllers/categoryController.js';
 
 // Brand CRUD
 router.get("/brand/store", brandController.store);
 router.post("/brand/show", brandController.show);
 router.put("/brand/update", brandController.update);
 router.delete("/brand/destroy", brandController.destroy);
+
+// ToDo CRUD
+router.get("/todo/store", todoController.store);
+
+// Category CRUD
+router.get("/category/store", categoryController.store);
 
 router.post("/todo/store", todoController.store);
 router.get("/todo/show", todoController.show);
